7fe1b9bba283343f7ba86dc7c313c2968719147b
FortySixAnd2 <jmaxj91j@gmail.com>
2025-03-05 13:29:23 +0300

    style: Remove comments.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,6 @@
 import { TOTAL_OFFERS_COUNT } from '../../const';
 import MainPage from '../../pages/main-page/main-page';
 
-// import { offerMock } from '../../mock/offer-mock';
-// import OfferPage from '../../pages/offer-page/offer-page';
-
-// import FavoritesPage from '../../pages/favorites-page/favorites-page';
-
 export default function App(): JSX.Element {
   return <MainPage offersCount={TOTAL_OFFERS_COUNT} />;
-  // return <OfferPage offer={offerMock} />;
-  // return <FavoritesPage />;
 }
