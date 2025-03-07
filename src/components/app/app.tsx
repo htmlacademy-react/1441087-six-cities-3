709d8e8fd9840fa586a84ac3709e1df43c7682b8
FortySixAnd2 <jmaxj91j@gmail.com>
2025-03-07 18:05:42 +0300

    refactor: Connect PrivateRoute component for FavoritesPage.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,12 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import { AppRoute, TOTAL_OFFERS_COUNT } from '../../const';
+import { AppRoute, AuthorizationStatus, TOTAL_OFFERS_COUNT } from '../../const';
 import { offerMock } from '../../mock/offer-mock';
 import MainPage from '../../pages/main-page/main-page';
 import LoginPage from '../../pages/login-page/login-page';
 import OfferPage from '../../pages/offer-page/offer-page';
 import FavoritesPage from '../../pages/favorites-page/favorites-page';
 import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import PrivateRoute from '../private-route/private-route';
 
 export default function App(): JSX.Element {
   return (
@@ -20,7 +21,14 @@ export default function App(): JSX.Element {
           path={AppRoute.Offer}
           element={<OfferPage offer={offerMock} />}
         />
-        <Route path={AppRoute.Favorites} element={<FavoritesPage />} />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <FavoritesPage />
+            </PrivateRoute>
+          }
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
