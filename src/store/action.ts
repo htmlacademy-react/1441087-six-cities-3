6737a516015029ca956e1f49a6706505ae169e82
FortySixAnd2 <jmaxj91j@gmail.com>
2025-04-09 12:57:45 +0300

    refactor: Simplify Redux actions definition.

--- src/store/action.ts
+++ src/store/action.ts
@@ -2,15 +2,7 @@ import { createAction } from '@reduxjs/toolkit';
 import { City } from '../types/city';
 import { SortOptionType } from '../components/sort/types';
 
-const setCity = createAction('city/setCity', (city: City) => ({
-  payload: city,
-}));
-
-const setSortOption = createAction(
-  'city/setSortOption',
-  (sortOption: SortOptionType) => ({
-    payload: sortOption,
-  })
-);
+const setCity = createAction<City>('city/setCity');
+const setSortOption = createAction<SortOptionType>('city/setSortOption');
 
 export { setCity, setSortOption };
