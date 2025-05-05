6d3dda2098604f434871391086661850ffa95f3f
FortySixAnd2 <jmaxj91j@gmail.com>
2025-05-05 15:37:39 +0300

    refactor: Add createSelector for full offer slice.

--- src/store/slices/offers-slice/selectors.ts
+++ src/store/slices/offers-slice/selectors.ts
@@ -1,19 +1,37 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { NameSpace } from '../../../const/store-const';
 import { State } from '../../../types/store-types';
 import { sortOfferPreviews } from '../../../components/sort/utils';
 import { getCityOffers } from '../../../utils/city-utils';
-import { createSelector } from '@reduxjs/toolkit';
 
 const selectSelf = (state: State) => state[NameSpace.Offers];
 
 export const selectCity = createSelector(selectSelf, (state) => state.city);
-export const selectSortOption = createSelector(selectSelf, (state) => state.sortOption);
 
-export const selectOfferPreviews = createSelector(selectSelf,(state) => state.offerPreviews);
-export const selectOfferPreviewsStatus = createSelector(selectSelf, (state) => state.offerPreviewsStatus);
+export const selectSortOption = createSelector(
+  selectSelf,
+  (state) => state.sortOption
+);
 
-export const selectFavoriteOfferPreviews = createSelector(selectSelf, (state) => state.favoriteOfferPreviews);
-export const selectFavoriteOfferPreviewsStatus = createSelector(selectSelf, (state) => state.favoriteOfferPreviewsStatus);
+export const selectOfferPreviews = createSelector(
+  selectSelf,
+  (state) => state.offerPreviews
+);
+
+export const selectOfferPreviewsStatus = createSelector(
+  selectSelf,
+  (state) => state.offerPreviewsStatus
+);
+
+export const selectFavoriteOfferPreviews = createSelector(
+  selectSelf,
+  (state) => state.favoriteOfferPreviews
+);
+
+export const selectFavoriteOfferPreviewsStatus = createSelector(
+  selectSelf,
+  (state) => state.favoriteOfferPreviewsStatus
+);
 
 export const selectMainOffers = createSelector(selectSelf, (state) => {
   const currentCity = state.city;
