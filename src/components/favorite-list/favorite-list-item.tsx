c94a227b627ccdc7af301c4b7f88972676bda8b3
FortySixAnd2 <jmaxj91j@gmail.com>
2025-05-06 13:14:47 +0300

    feat: Add navigate to the main page from the favorite page.

--- src/components/favorite-list/favorite-list-item.tsx
+++ src/components/favorite-list/favorite-list-item.tsx
@@ -1,4 +1,8 @@
+import { Link } from 'react-router-dom';
+import { AppRoute, CITIES } from '../../const/app-const';
+import { offersActions } from '../../store/slices/offers-slice/offers-slice';
 import { OfferPreviews } from '../../types/offer-types';
+import useAppDispatch from '../../hooks/use-app-dispatch';
 import OfferCardSmall from '../offer-card-small';
 
 type FavoriteListItemProps = {
@@ -8,22 +12,28 @@ type FavoriteListItemProps = {
 
 function FavoriteListItem(props: FavoriteListItemProps): JSX.Element {
   const { cityName, offerPreviews } = props;
+  const dispatch = useAppDispatch();
+
+  const handleCityClick = () => {
+    dispatch(offersActions.setCity(CITIES[cityName]));
+  };
 
   return (
     <li className="favorites__locations-items">
       <div className="favorites__locations locations locations--current">
         <div className="locations__item">
-          <a className="locations__item-link" href="#">
+          <Link
+            className="locations__item-link"
+            to={AppRoute.Root}
+            onClick={handleCityClick}
+          >
             <span>{cityName}</span>
-          </a>
+          </Link>
         </div>
       </div>
       <div className="favorites__places">
         {offerPreviews.map((offerPreview) => (
-          <OfferCardSmall
-            key={offerPreview.id}
-            offerPreview={offerPreview}
-          />
+          <OfferCardSmall key={offerPreview.id} offerPreview={offerPreview} />
         ))}
       </div>
     </li>
