b855b8cfe1b6b5b68ca64a91019f03c004763238
FortySixAnd2 <jmaxj91j@gmail.com>
2025-05-11 12:55:53 +0300

    test: Add tests for full-offer-slice.

--- src/store/slices/full-offer-slice/full-offer-slice.test.ts
+++ src/store/slices/full-offer-slice/full-offer-slice.test.ts
@@ -1,25 +1,295 @@
 import { RequestStatus } from '../../../const/api-const';
-import { fullOfferReducer } from './full-offer-slice';
+import { NewReview, Review } from '../../../types/review-types';
+import {
+  getMockOfferFull,
+  getMockOfferPreviews,
+  getMockReviews,
+} from '../../../utils/mock-utils';
+import { fullOfferActions, fullOfferReducer } from './full-offer-slice';
 
 describe('Full Offer Slice', () => {
+  const initialState = {
+    offerFull: null,
+    offerFullStatus: RequestStatus.Idle,
+    nearOfferPreviews: [],
+    nearOfferPreviewsStatus: RequestStatus.Idle,
+    reviews: [],
+    reviewsStatus: RequestStatus.Idle,
+    postReviewStatus: RequestStatus.Idle,
+  };
+
   it('should return initial state with empty action', () => {
     const emptyAction = { type: '' };
-    const initialState = {
-      offerFull: null,
-      offerFullStatus: RequestStatus.Idle,
-
-      nearOfferPreviews: [],
-      nearOfferPreviewsStatus: RequestStatus.Idle,
-
-      reviews: [],
-      reviewsStatus: RequestStatus.Idle,
-
-      postReviewStatus: RequestStatus.Idle,
-    };
     const expectedState = initialState;
 
     const result = fullOfferReducer(expectedState, emptyAction);
 
     expect(result).toEqual(expectedState);
+  });
+
+  it('should return initial state with undefined state and empty action', () => {
+    const emptyAction = { type: '' };
+    const expectedState = initialState;
+
+    const result = fullOfferReducer(undefined, emptyAction);
+
+    expect(result).toEqual(expectedState);
+  });
+
+  describe('Get Full Offer', () => {
+    it('should set "offerFullStatus" to "Loading" with "getOfferFull.pending"', () => {
+      const expectedState = {
+        offerFull: null,
+        offerFullStatus: RequestStatus.Loading,
+        nearOfferPreviews: [],
+        nearOfferPreviewsStatus: RequestStatus.Idle,
+        reviews: [],
+        reviewsStatus: RequestStatus.Idle,
+        postReviewStatus: RequestStatus.Idle,
+      };
+
+      const result = fullOfferReducer(
+        undefined,
+        fullOfferActions.getOfferFull.pending
+      );
+
+      expect(result).toEqual(expectedState);
+    });
+
+    it('should set "offerFullStatus" to "Success" and fill in offerFull with "getOfferFull.fulfilled"', () => {
+      const mockOfferFull = getMockOfferFull();
+      const expectedState = {
+        offerFull: mockOfferFull,
+        offerFullStatus: RequestStatus.Success,
+        nearOfferPreviews: [],
+        nearOfferPreviewsStatus: RequestStatus.Idle,
+        reviews: [],
+        reviewsStatus: RequestStatus.Idle,
+        postReviewStatus: RequestStatus.Idle,
+      };
+
+      const result = fullOfferReducer(
+        undefined,
+        fullOfferActions.getOfferFull.fulfilled(mockOfferFull, '', '', '')
+      );
+
+      expect(result).toEqual(expectedState);
+    });
+
+    it('should set "offerFullStatus" to "Failed" with "getOfferFull.rejected"', () => {
+      const expectedState = {
+        offerFull: null,
+        offerFullStatus: RequestStatus.Failed,
+        nearOfferPreviews: [],
+        nearOfferPreviewsStatus: RequestStatus.Idle,
+        reviews: [],
+        reviewsStatus: RequestStatus.Idle,
+        postReviewStatus: RequestStatus.Idle,
+      };
+
+      const result = fullOfferReducer(
+        undefined,
+        fullOfferActions.getOfferFull.rejected
+      );
+
+      expect(result).toEqual(expectedState);
+    });
+  });
+
+  describe('Get Near Offer Previews', () => {
+    it('should set "nearOfferPreviewsStatus" to "Loading" with "getNearOfferPreviews.pending"', () => {
+      const expectedState = {
+        offerFull: null,
+        offerFullStatus: RequestStatus.Idle,
+        nearOfferPreviews: [],
+        nearOfferPreviewsStatus: RequestStatus.Loading,
+        reviews: [],
+        reviewsStatus: RequestStatus.Idle,
+        postReviewStatus: RequestStatus.Idle,
+      };
+
+      const result = fullOfferReducer(
+        undefined,
+        fullOfferActions.getNearOfferPreviews.pending
+      );
+
+      expect(result).toEqual(expectedState);
+    });
+
+    it('should set "nearOfferPreviewsStatus" to "Success" and fill in nearOfferPreviews with "getOfferFull.fulfilled"', () => {
+      const mockOfferPreviews = getMockOfferPreviews();
+      const expectedState = {
+        offerFull: null,
+        offerFullStatus: RequestStatus.Idle,
+        nearOfferPreviews: mockOfferPreviews,
+        nearOfferPreviewsStatus: RequestStatus.Success,
+        reviews: [],
+        reviewsStatus: RequestStatus.Idle,
+        postReviewStatus: RequestStatus.Idle,
+      };
+
+      const result = fullOfferReducer(
+        undefined,
+        fullOfferActions.getNearOfferPreviews.fulfilled(
+          mockOfferPreviews,
+          '',
+          '',
+          ''
+        )
+      );
+
+      expect(result).toEqual(expectedState);
+    });
+
+    it('should set "nearOfferPreviewsStatus" to "Failed" with "getNearOfferPreviews.rejected"', () => {
+      const expectedState = {
+        offerFull: null,
+        offerFullStatus: RequestStatus.Idle,
+        nearOfferPreviews: [],
+        nearOfferPreviewsStatus: RequestStatus.Failed,
+        reviews: [],
+        reviewsStatus: RequestStatus.Idle,
+        postReviewStatus: RequestStatus.Idle,
+      };
+
+      const result = fullOfferReducer(
+        undefined,
+        fullOfferActions.getNearOfferPreviews.rejected
+      );
+
+      expect(result).toEqual(expectedState);
+    });
+  });
+
+  describe('Get Reviews', () => {
+    it('should set "reviewsStatus" to "Loading" with "getReviews.pending"', () => {
+      const expectedState = {
+        offerFull: null,
+        offerFullStatus: RequestStatus.Idle,
+        nearOfferPreviews: [],
+        nearOfferPreviewsStatus: RequestStatus.Idle,
+        reviews: [],
+        reviewsStatus: RequestStatus.Loading,
+        postReviewStatus: RequestStatus.Idle,
+      };
+
+      const result = fullOfferReducer(
+        undefined,
+        fullOfferActions.getReviews.pending
+      );
+
+      expect(result).toEqual(expectedState);
+    });
+
+    it('should set "reviewsStatus" to "Success" and fill in reviews with "getReviews.fulfilled"', () => {
+      const mockReviews = getMockReviews();
+      const expectedState = {
+        offerFull: null,
+        offerFullStatus: RequestStatus.Idle,
+        nearOfferPreviews: [],
+        nearOfferPreviewsStatus: RequestStatus.Idle,
+        reviews: mockReviews,
+        reviewsStatus: RequestStatus.Success,
+        postReviewStatus: RequestStatus.Idle,
+      };
+
+      const result = fullOfferReducer(
+        undefined,
+        fullOfferActions.getReviews.fulfilled(mockReviews, '', '', '')
+      );
+
+      expect(result).toEqual(expectedState);
+    });
+
+    it('should set "reviewsStatus" to "Failed" with "getReviews.rejected"', () => {
+      const expectedState = {
+        offerFull: null,
+        offerFullStatus: RequestStatus.Idle,
+        nearOfferPreviews: [],
+        nearOfferPreviewsStatus: RequestStatus.Idle,
+        reviews: [],
+        reviewsStatus: RequestStatus.Failed,
+        postReviewStatus: RequestStatus.Idle,
+      };
+
+      const result = fullOfferReducer(
+        undefined,
+        fullOfferActions.getReviews.rejected
+      );
+
+      expect(result).toEqual(expectedState);
+    });
+  });
+
+  describe('Post Review', () => {
+    it('should set "postReviewStatus" to "Loading" with "postReview.pending"', () => {
+      const expectedState = {
+        offerFull: null,
+        offerFullStatus: RequestStatus.Idle,
+        nearOfferPreviews: [],
+        nearOfferPreviewsStatus: RequestStatus.Idle,
+        reviews: [],
+        reviewsStatus: RequestStatus.Idle,
+        postReviewStatus: RequestStatus.Loading,
+      };
+
+      const result = fullOfferReducer(
+        undefined,
+        fullOfferActions.postReview.pending
+      );
+
+      expect(result).toEqual(expectedState);
+    });
+
+    it('should set "postReviewStatus" to "Success" and fill in review with "postReview.fulfilled"', () => {
+      const mockReview: Review = getMockReviews()[0];
+      const mockNewReview: NewReview = {
+        rating: mockReview.rating,
+        comment: mockReview.comment,
+      };
+      const expectedState = {
+        offerFull: null,
+        offerFullStatus: RequestStatus.Idle,
+        nearOfferPreviews: [],
+        nearOfferPreviewsStatus: RequestStatus.Idle,
+        reviews: [mockReview],
+        reviewsStatus: RequestStatus.Idle,
+        postReviewStatus: RequestStatus.Success,
+      };
+
+      const result = fullOfferReducer(
+        undefined,
+        fullOfferActions.postReview.fulfilled(
+          mockReview,
+          '',
+          {
+            offerId: 'q1w2e3',
+            review: mockNewReview,
+          },
+          ''
+        )
+      );
+
+      expect(result).toEqual(expectedState);
+    });
+
+    it('should set "postReviewStatus" to "Failed" with "postReview.rejected"', () => {
+      const expectedState = {
+        offerFull: null,
+        offerFullStatus: RequestStatus.Idle,
+        nearOfferPreviews: [],
+        nearOfferPreviewsStatus: RequestStatus.Idle,
+        reviews: [],
+        reviewsStatus: RequestStatus.Idle,
+        postReviewStatus: RequestStatus.Failed,
+      };
+
+      const result = fullOfferReducer(
+        undefined,
+        fullOfferActions.postReview.rejected
+      );
+
+      expect(result).toEqual(expectedState);
+    });
   });
 });
---
+++ src/utils/mock-utils.ts
@@ -0,0 +1,133 @@
+import { OfferFull, OfferPreviews } from '../types/offer-types';
+import { Reviews } from '../types/review-types';
+
+export const getMockOfferPreviews = (): OfferPreviews => [
+  {
+    id: 'e5a2a853-caba-47a1-b63e-c9c649021122',
+    title: 'House in countryside',
+    type: 'room',
+    price: 195,
+    previewImage: 'https://16.design.htmlacademy.pro/static/hotel/5.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13,
+      },
+    },
+    location: {
+      latitude: 48.858610000000006,
+      longitude: 2.330499,
+      zoom: 16,
+    },
+    isFavorite: true,
+    isPremium: false,
+    rating: 2.3,
+  },
+  {
+    id: '40c73477-9799-4da0-997b-bf284d2b13bf',
+    title: 'Nice, cozy, warm big bed apartment',
+    type: 'room',
+    price: 123,
+    previewImage: 'https://16.design.htmlacademy.pro/static/hotel/15.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13,
+      },
+    },
+    location: {
+      latitude: 48.834610000000005,
+      longitude: 2.335499,
+      zoom: 16,
+    },
+    isFavorite: false,
+    isPremium: true,
+    rating: 3.9,
+  },
+];
+
+export const getMockOfferFull = (): OfferFull => ({
+  id: '89b6ae84-2724-4369-ab85-6877dbdd2795',
+  title: 'Perfectly located Castro',
+  description:
+    'Design interior in most sympathetic area! Complitely renovated, well-equipped, cosy studio in idyllic, over 100 years old wooden house. Calm street, fast connection to center and airport.',
+  type: 'room',
+  price: 299,
+  images: [
+    'https://16.design.htmlacademy.pro/static/hotel/14.jpg',
+    'https://16.design.htmlacademy.pro/static/hotel/9.jpg',
+    'https://16.design.htmlacademy.pro/static/hotel/20.jpg',
+    'https://16.design.htmlacademy.pro/static/hotel/7.jpg',
+    'https://16.design.htmlacademy.pro/static/hotel/6.jpg',
+    'https://16.design.htmlacademy.pro/static/hotel/2.jpg',
+  ],
+  city: {
+    name: 'Paris',
+    location: {
+      latitude: 48.85661,
+      longitude: 2.351499,
+      zoom: 13,
+    },
+  },
+  location: {
+    latitude: 48.868610000000004,
+    longitude: 2.342499,
+    zoom: 16,
+  },
+  goods: [
+    'Air conditioning',
+    'Wi-Fi',
+    'Laptop friendly workspace',
+    'Towels',
+    'Washing machine',
+    'Kitchen',
+    'Baby seat',
+    'Breakfast',
+    'Fridge',
+    'Washer',
+    'Dishwasher',
+    'Coffee machine',
+  ],
+  host: {
+    isPro: true,
+    name: 'Angelina',
+    avatarUrl:
+      'https://16.design.htmlacademy.pro/static/host/avatar-angelina.jpg',
+  },
+  isPremium: true,
+  isFavorite: false,
+  rating: 3.4,
+  bedrooms: 1,
+  maxAdults: 2,
+});
+
+export const getMockReviews = (): Reviews => [
+  {
+    id: 'ecefb027-adde-4aa7-b53d-6f35093252dd',
+    comment:
+      'What an amazing view! The house is stunning and in an amazing location. The large glass wall had an amazing view of the river!',
+    date: '2025-02-10T21:00:00.468Z',
+    rating: 5,
+    user: {
+      name: 'Mollie',
+      avatarUrl: 'https://16.design.htmlacademy.pro/static/avatar/6.jpg',
+      isPro: false,
+    },
+  },
+  {
+    id: 'a127e283-903e-4e1b-a5d8-75008d0039c1',
+    comment:
+      'Home is amazing. It\'s like staying in a museum. The rooms, furnishings and artworks are incredible. The views of My Vesuvius',
+    date: '2025-02-07T21:00:00.468Z',
+    rating: 2,
+    user: {
+      name: 'Jack',
+      avatarUrl: 'https://16.design.htmlacademy.pro/static/avatar/7.jpg',
+      isPro: true,
+    },
+  },
+];
