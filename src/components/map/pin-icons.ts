ddd5aa187e3c83ec7545dd90e6d098c4ae44f0a3
FortySixAnd2 <jmaxj91j@gmail.com>
2025-05-21 10:37:30 +0300

    refactor: Change path to the map pins icons.

--- src/components/map/pin-icons.ts
+++ src/components/map/pin-icons.ts
@@ -1,7 +1,7 @@
 import leaflet from 'leaflet';
 
-const URL_PIN_DEFAULT = '../../../public/img/pin.svg';
-const URL_PIN_ACTIVE = '../../../public/img/pin-active.svg';
+const URL_PIN_DEFAULT = '/img/pin.svg';
+const URL_PIN_ACTIVE = '/img/pin-active.svg';
 
 export const defaultCustomIcon = leaflet.icon({
   iconUrl: URL_PIN_DEFAULT,
